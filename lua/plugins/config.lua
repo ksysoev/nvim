2ee037f4939c352ac0696047de941c3c6434f57d
Kirill Sysoev <k.sysoev@me.com>
2025-08-09 23:22:59 +0800

    Adds octo for working with github, and change moddel for for copilot

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -14,4 +14,15 @@ return {
         },
     },
     { "ThePrimeagen/vim-be-good" },
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = {
+            { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+        },
+        opts = {
+            model = "claude-sonnet-4",
+            -- See Configuration section for options
+        },
+        -- See Commands section for default commands if you want to lazy load on them
+    },
 }
